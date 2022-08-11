7b4fa387b0e52d2d2341ca26b5cde84131893249
Alan Rezende <alan.rezende@boxtecnologia.local>
2022-08-11 07:43:50 -0300

    diversos #alan.rezende

--- src/BoxBack.WebApi/EndPoints/User/UsersEndpoint.cs
+++ src/BoxBack.WebApi/EndPoints/User/UsersEndpoint.cs
@@ -62,13 +62,13 @@ namespace BoxBack.WebApi.EndPoints.User
         /// <response code="200">Lista de usuários</response>
         /// <response code="400">Lista nula</response>
         /// <response code="404">Lista vazia</response>
-         [AllowAnonymous]
+        [Authorize(Roles = "MASTER, USER_LIST")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [Route("list")]
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll(string q)
         {
             var result = new List<UserTemp>();
             try
@@ -78,7 +78,7 @@ namespace BoxBack.WebApi.EndPoints.User
                     Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
                     FullName = "ALAN LEITE DE REZENDE",
                     Company = "Box Tecnologia",
-                    Role = "admin",
+                    Roles = new string[] {"SUPORTE", "FINANCEIRO"},
                     UserName = "alan.rezende",
                     Country = "Brasil",
                     Contact = "(48) 9.9961-6679",
@@ -88,7 +88,39 @@ namespace BoxBack.WebApi.EndPoints.User
                     AvatarColor = "primary"
                 };
 
+                var user2 = new UserTemp()
+                {
+                    Id = "43ab3050-cfe7-4f6b-9034-d9d4c317df0e",
+                    FullName = "JOÃO DA SILVA",
+                    Company = "Box Tecnologia",
+                    Roles = new string[] {"MASTER", "ADMIN", "SUPORTE", "USER"},
+                    UserName = "joao.silva",
+                    Country = "Brasil",
+                    Contact = "(48) 9.9961-6679",
+                    Email = "joao.silva@boxtecnologia.com.br",
+                    Status = "PENDING",
+                    Avatar = "",
+                    AvatarColor = "info"
+                };
+
+                var user3 = new UserTemp()
+                {
+                    Id = "a94c13b5-756a-467d-8f18-6b10254c7f3e",
+                    FullName = "LEONARDO CARVALHO MOREIRA",
+                    Company = "Box Tecnologia",
+                    Roles = new string[] {"MASTER", "ADMIN", "SUPORTE", "USER"},
+                    UserName = "leonardo.moreira",
+                    Country = "Brasil",
+                    Contact = "(48) 9.9961-6679",
+                    Email = "leonardo.moreira@boxtecnologia.com.br",
+                    Status = "INACTIVE",
+                    Avatar = "",
+                    AvatarColor = "warning"
+                };
+
                 result.Add(user1);
+                result.Add(user2);
+                result.Add(user3);
 
                 // users = await _manager.Users
                 //                     .AsNoTracking()
@@ -99,6 +131,10 @@ namespace BoxBack.WebApi.EndPoints.User
             catch (Exception ex){ return StatusCode(500, ex.Message); }
             if (result == null)
                 return StatusCode(404, "Not found.");
+
+            if(!string.IsNullOrEmpty(q))
+                result = result.Where(x => x.FullName.Contains(q.ToUpper())).ToList();
+
             return Ok(new {
                 AllData = result,
                 Users = result,
@@ -112,7 +148,7 @@ namespace BoxBack.WebApi.EndPoints.User
             public string Id { get; set; }
             public string FullName { get; set; }
             public string Company { get; set; }
-            public string Role { get; set; }
+            public new string[] Roles { get; set; }
             public string UserName { get; set; }
             public string Country { get; set; }
             public string Contact { get; set; }
