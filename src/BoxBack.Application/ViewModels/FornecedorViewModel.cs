8b76e5f351f3e50bb180f83da8aa377d7f5042d2
sigespweb22 <sigespweb22@gmail.com>
2022-10-10 16:11:03 -0300

    implementações on issue #10

---
+++ src/BoxBack.Application/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,44 @@
+using System.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using BoxBack.Domain.Enums;
+using BoxBack.Domain.Models;
+using BoxBack.Application.ViewModels;
+
+
+namespace BoxBack.Application.ViewModels
+{
+    public class FornecedorViewModel
+    {
+        public Guid? Id { get; set; }
+
+        [Required(ErrorMessage = "\nNome fantasia é requerido.")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "\nNome fantasia deve possuir entre 3 e 255 caracteres.")]
+        public string NomeFantasia { get; set; }
+
+        [Required(ErrorMessage = "\nRazão social é requerida.")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "\nRazão social deve possuir entre 3 e 255 caracteres.")]
+        public string RazaoSocial { get; set; }
+        
+        [StringLength(14, MinimumLength = 0, ErrorMessage = "\nInscrição estadual deve possuir 14 caracteres.")]
+        public string InscricaoEstadual { get; set; }
+
+        [Required(ErrorMessage = "\nCNPJ é requerido.")]
+        [StringLength(18, MinimumLength = 14, ErrorMessage = "\nCNPJ deve possuir 14 caracteres.")]
+        public string CNPJ { get; set; }
+        public string TelefonePrincipal { get; set; }
+        public string EmailPrincipal { get; set; }
+        public string Observacao { get; set; }
+        private int _codigoMunicipio = 0;
+        public int CodigoMunicio { get { return _codigoMunicipio; }  set { _codigoMunicipio = value; }}
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+        public string Status { get; set; }
+    }
+}
--- src/BoxBack.Domain/Models/Fornecedor.cs
+++ src/BoxBack.Domain/Models/Fornecedor.cs
@@ -8,18 +8,25 @@ namespace BoxBack.Domain.Models
 {
     public class Fornecedor : EntityAudit
     {        
-        public Fornecedor(string nomeFantasia, string razaoSocial,
+        public Fornecedor(string nomeFantasia, string razaoSocial, string inscricaoEstadual,
                           string cnpj, string telefonePrincipal, string emailPrincipal,
-                          string observacao, string cidade, string estado)
+                          int codigoMunicipio, string rua, string numero, string complemento,
+                          string cidade, string estado, string cep, string observacao)
         {
             NomeFantasia = nomeFantasia;
             RazaoSocial = razaoSocial;
-            Cnpj = cnpj;
+            InscricaoEstadual = inscricaoEstadual;
+            CNPJ = cnpj;
             TelefonePrincipal = telefonePrincipal;
             EmailPrincipal = emailPrincipal;
-            Observacao = observacao;
+            CodigoMunicipio = codigoMunicipio;
+            Rua = rua;
+            Numero = numero;
+            Complemento = complemento;
             Cidade = cidade;
             Estado = estado;
+            Cep = cep;
+            Observacao = observacao;
         }
 
         // Constructor empty for EF
@@ -27,19 +34,23 @@ namespace BoxBack.Domain.Models
 
         public string NomeFantasia { get; set; }
         public string RazaoSocial { get; set; }
-        public string Cnpj { get; set; }
+        public string InscricaoEstadual { get; set; }
+        public string CNPJ { get; set; }
         public string TelefonePrincipal { get; set; }
         public string EmailPrincipal { get; set; }
-        public string Observacao { get; set; }
+        public int? CodigoMunicipio { get; set; }
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
         public string Cidade { get; set; }
         public string Estado { get; set; }
+        public string Cep { get; set; }
+        public string Observacao { get; set; }
 
 
         // Relationships
         [ForeignKey("TenantId")]
         public Guid TenantId { get; set; }
         public Tenant Tenant { get; set; }
-
-        public ICollection<FornecedorSolucao> FornecedorSolucoes { get; set; }
     }
 }
