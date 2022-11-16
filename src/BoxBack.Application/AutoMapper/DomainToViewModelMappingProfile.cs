8817fe281cbdef057169e00d1f5547eb7cde7c13
sigespweb22 <sigespweb22@gmail.com>
2022-11-16 09:13:24 -0300

    implementações diversas on issue #30

--- src/BoxBack.Application/AutoMapper/DomainToViewModelMappingProfile.cs
+++ src/BoxBack.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -61,7 +61,8 @@ namespace BoxBack.Application.AutoMapper
                 .ForMember(dst => dst.ApplicationUserGroups, src => src.MapFrom(x => x.ApplicationUserGroups));
             CreateMap<ApplicationUser, UsuarioInfoViewModel>();
             CreateMap<ChaveApiTerceiro, ChaveApiTerceiroViewModel>()
-                .ForMember(dst => dst.Status, src => src.MapFrom(x => x.IsDeleted ? "INACTIVE" : "ACTIVE"));
+                .ForMember(dst => dst.Status, src => src.MapFrom(x => x.IsDeleted ? "INACTIVE" : "ACTIVE"))
+                .ForMember(dst => dst.DataValidade, src => src.MapFrom(x => x.DataValidade.ToString("dd/MM/yyyy")));
             CreateMap<ChaveApiTerceiro, ChaveApiTerceiroSelect2ViewModel>();
         }
     }
