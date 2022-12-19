1aa28322e53b26928a09ca668ddabb7790c12283
sigespweb22 <sigespweb22@gmail.com>
2022-12-19 11:00:10 -0300

    Resolved on issue Closed #62

--- src/BoxBack.Domain/ModelsServices/BCContratoModelService.cs
+++ src/BoxBack.Domain/ModelsServices/BCContratoModelService.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System;
+using System.Collections.Generic;
 using BoxBack.Domain.Enums;
 
 namespace BoxBack.Domain.ModelsServices
@@ -20,5 +22,37 @@ namespace BoxBack.Domain.ModelsServices
         public string NomeIndiceReajuste { get; set; }
         public PeriodoReajusteEnum PeriodoReajuste { get; set; }
         public string NomePeriodoReajuste { get; set; }
+        public ICollection<BCFaturaModelService> Faturas { get; set; }
+    }
+
+    public class BCFaturaModelService
+    {
+        public Int32 Id { get; set; }
+        public Guid IdParcela { get; set; }
+        public Guid IdMovimentacaoFinanceira { get; set; }
+        public Int32 TipoFatura { get; set; }
+        public string NomeTipoFatura { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataCompetencia { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Desconto { get; set; }
+        public DateTime? DataPagamento { get; set; }
+        public decimal ValorPagamento { get; set; }
+        public Int32 NumeroParcela { get; set; }
+        public bool Quitado { get; set; }
+        public bool Conciliado { get; set; }
+        public Int32 FormaPagamento { get; set; }
+        public string NomeFormaPagamento { get; set; }
+        public bool EmiteBoleto{ get; set; }
+        public string ObservacaoBoleto { get; set; }
+        public Int32 IdBoleto { get; set; }
+        public string LinkBoleto { get; set; }
+        public bool EmiteNotaFiscal { get; set; }
+        public string ObservacaoNotaFiscal { get; set; }
+        public string LinkNotaFiscal { get; set; }
+        public string LinkXmlNotaFiscal { get; set; }
+        public DateTime DataPromessaPagamento { get; set; }
+        public bool ClienteNaoVaiPagar { get; set; }
+        public string Anexos { get; set; }
     }
 }
